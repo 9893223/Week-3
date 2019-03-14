d67871ce6a14fa61106c1e1f899efda0cab726d0
Benjamin De Rijk <9893223@student.toiohomai.ac.nz>
2019-03-14 14:03:15 +1300

    msg

---
+++ Task 2 Loops/Task 2 Loops/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 1;
+            int finish = 7;
+
+            for(int i = 1; i <= finish; i++)
+            {
+                Console.WriteLine($"{i}");
+
+            }
+
+        }
+    }
+}
---
+++ Task1 Loops/Task1 Loops/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task1_Loops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Title//
+            Console.WriteLine();
+            int start = 3;
+            int finish = 7;
+
+            for(int i = 3; i <= finish; i++)
+            {
+                Console.WriteLine($" {i}\t\t{i * 10}\t\t{i * 100}\t\t{i * 1000}");
+            }
+
+            int counter = 3;
+            finish = 7;
+            while (counter <= finish)
+            {
+                Console.WriteLine($"{counter}\t\t{counter * 10}\t\t{counter * 100}\t\t{counter * 1000}"); counter++;
+            }
+
+            counter = 3;
+            do
+            {
+
+                Console.WriteLine($"{counter}\t\t{counter * 10}\t\t{counter * 100}\t\t{counter * 1000}"); counter++;
+
+            } while (counter <= finish);
+
+
+            Console.ReadLine();
+        }
+    }
+}
